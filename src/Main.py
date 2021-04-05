8b1f9a671fe260b610e7c34916349141a5f34c22
mfikrin <muhammad.fikrin1729@gmail.com>
2021-04-05 11:48:46 +0700

    add fungsi display_mat global

--- src/Main.py
+++ src/Main.py
@@ -11,11 +11,11 @@ def addEdge(adj, u, v):
     adj[u].append(v)
     adj[v].append(u)
 
-# def display_mat(matriks):
-#     for i in range(len(matriks):
-#         for j in range(len(matriks)):
-#             print(matriks[i][j], end="\t")
-#         print()
+def display_mat(matriks,n_row,n_col):
+    for i in range(n_row):
+        for j in range(n_col):
+            print(matriks[i][j], end="\t")
+        print()
 
 class Graf:
     
@@ -66,18 +66,6 @@ file_edge = open(path2, "r")
 data = file_data.read().splitlines() # baca file teks (dengan readlines yang sekaligus hapus \n)
 edge = file_edge.read().splitlines()
 
-
-
-
-# print(data)
-
-# for i in range(len(data)):
-#     data[i] = data[i].replace("\n", "")
-
-
-
-
-
 n_node = int(data[0])
 print("n node ", n_node)
 data.remove(data[0])
@@ -85,10 +73,10 @@ int_data = [[0 for j in range(2)] for i in range(len(data))]
 for i in range(len(data)):
     string = data[i][2:len(data[i])]
     x = string.split(",")
-    print(string)
-    print(x)
-    print(x[0])
-    print(x[1])
+    # print(string)
+    # print(x)
+    # print(x[0])
+    # print(x[1])
 
     int_data[i][0] = int(x[0])
     int_data[i][1] = int(x[1])
@@ -98,7 +86,7 @@ mat_edge = [[0 for j in range(n_node)] for i in range(n_node)]
 
 baris = 0
 kolom = 0
-print(edge)
+# print(edge)
 for i in range(len(edge)):
     # print(len(file[i]))
     # print(str(i) + "woi")
@@ -111,39 +99,39 @@ for i in range(len(edge)):
     kolom = 0
 
 
-for i in range(8):
-    for j in range(8):
-        print(mat_edge[i][j], end=" ")
-    print()
+# for i in range(8):
+#     for j in range(8):
+#         print(mat_edge[i][j], end=" ")
+#     print()
 
+display_mat(mat_edge,n_node,n_node)
 
+# for i in range(len(int_data)):
+#     for j in range(2):
+#         print(int_data[i][j],end= " ")
+#     print()
 
-
-
-for i in range(len(int_data)):
-    for j in range(2):
-        print(int_data[i][j],end= " ")
-    print()
+display_mat(int_data,n_node,2)
 
 g = Graf(n_node)
-g.display_matrix()
+# g.display_matrix()
 
-print(data)
-    
-
+# print(data)
+#
+# print()
 for i in range(len(data)):
     for j in range(i+1,len(data)):
         m = int_data[i]
         n = int_data[j]
-        print(m, end=" ")
-        print(n, end=" ")
+        # print(m, end=" ")
+        # print(n, end=" ")
 
         x1 = m[0]
         y1 = m[1]
         x2 = n[0]
         y2 = n[1]
         jarak = Euclidean(x1,x2,y1,y2)
-        print(jarak)
+        # print(jarak)
         # matriks_adj[i][j] = jarak
         g.add_element(i,j,jarak)
         # matriks_adj[j][i] = jarak
@@ -161,9 +149,10 @@ for i in range(8):
             mat_mix[i][j] = g.matrix[i][j]
 
 
-for i in range(8):
-    for j in range(8):
-        print(mat_mix[i][j], end="\t")
-    print()
+# for i in range(8):
+#     for j in range(8):
+#         print(mat_mix[i][j], end="\t")
+#     print()
 
+display_mat(mat_mix,n_node,n_node)
 
